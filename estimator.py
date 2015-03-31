901ec24a325f9b0dee88849caa450c5d087f846b
Jonathan Challinger <mr.challinger@gmail.com>
2015-03-31 10:17:21 -0700

    more changes

--- estimator.py
+++ estimator.py
@@ -10,11 +10,10 @@ class ExtendedKalmanFilter:
         I = np.matrix(np.eye(P.shape[0]))
         x_hat = self.state
 
-        print x_hat
-
         y_tilde = meas - self.h()
         S = H * P * H.T + R
         K = P * H.T * S.I
+
         x_hat = x_hat + K*y_tilde
         P = (I-K*H)*P
 
@@ -25,22 +24,26 @@ class ExtendedKalmanFilter:
         P = self.covariance
         F = self.F(dt)
         Q = self.Q(dt)
-        B = self.B(dt) if u is not None else None
-        x_hat = self.state
 
-        x_hat = F*x_hat
-        if B is not None and u is not None:
-            x_hat += B*u
+        x_hat = self.f(dt, u)
         P = F*P*F.T+Q
 
         self.state = x_hat
         self.covariance = P
 
-    def F(self, dt):
-        return np.matrix(np.eye(self.state.shape[0]))
-
     def h(self, dt):
         return H()*self.state
+
+    def f(self, dt, u):
+        B = self.B(dt) if u is not None else None
+
+        x_hat = self.state
+
+        x_hat = F*x_hat
+        if B is not None and u is not None:
+            x_hat += B*u
+
+        return x_hat
 
 
 class BattEstimator(ExtendedKalmanFilter):
@@ -49,36 +52,53 @@ class BattEstimator(ExtendedKalmanFilter):
         # x hat on wikipedia
         self.state = np.matrix([
             [ 0 ], #Vo
-            [ 0 ],    #I
+            [ 0 ], #I
             [ 0 ]  #R
             ])
 
         # P on wikipedia
         self.covariance = np.matrix([
-            [ 20**2 , 0 , 0 ],
-            [ 0 , 5**2 , 0 ],
-            [ 0 , 0 , 0.02**2 ]
+            [ 20**2 ,   0   ,    0   ],
+            [   0   , 1**2  ,    0   ],
+            [   0   ,   0   , 0.2**2 ]
             ])
+
+    def f(self, dt, u):
+        Vo = self.state.item(0)
+        I  = self.state.item(1)
+        R  = self.state.item(2)
+
+        if I < 0:
+            I = 0
+
+        if R < 0:
+            R = 0
+
+        return np.matrix([
+            [ Vo ],
+            [ I  ],
+            [ R  ]
+            ])
+
+
+    def F(self, dt):
+        return np.matrix(np.eye(self.state.shape[0]))
 
     def R(self):
-        meas_noise = np.matrix([
-            [ 0.1**2 ],
-            [ 0.1**2 ]
+        return np.matrix([
+            [ 0.1**2 ,    0   ],
+            [    0   , 0.1**2 ]
             ])
-        return meas_noise*meas_noise.T
 
     def Q(self,dt):
-        VOLT_NOISE = 0.05
-        CURR_NOISE = 1400
-        RES_NOISE  = 0.02/60.0
+        V_PNOISE = 0.05
+        I_PNOISE = 1400
+        R_PNOISE  = 0.05/60.0
         return np.matrix([
-            [ (VOLT_NOISE*dt)**2 ,          0         ,          0        ],
-            [         0          , (CURR_NOISE*dt)**2 ,          0        ],
-            [         0          ,          0         , (RES_NOISE*dt)**2 ]
+            [ (V_PNOISE*dt)**2 ,         0        ,         0        ],
+            [         0        , (I_PNOISE*dt)**2 ,         0        ],
+            [         0        ,         0        , (R_PNOISE*dt)**2 ]
             ])
-
-    def F(self,dt):
-        return np.matrix(np.eye(self.state.shape[0]))
 
     def h(self):
         Vo = self.state.item(0)
